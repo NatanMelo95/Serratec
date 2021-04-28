e958b142b62cdecf9e6728c6653c2f38bea515d2
Natan Melo <natan.tere2012@gmail.com>
2021-04-28 10:09:31 -0300

    confirma

--- POO/exercicios/aula3/Dominio/src/main/java/br/com/wrsolucoes/bancodigital/model/cliente/Cliente.java
+++ POO/exercicios/aula3/Dominio/src/main/java/br/com/wrsolucoes/bancodigital/model/cliente/Cliente.java
@@ -1,4 +1,12 @@
 package main.java.br.com.wrsolucoes.bancodigital.model.cliente;
-public class Cliente {
 
+public class Cliente {
+    
+    public String nome;
+    public String cpfOuCnpj;
+    public String rg;
+    public String email;
+    public String celular;
+
+    
 }
---
+++ POO/exercicios/aula3/Dominio/src/main/java/br/com/wrsolucoes/bancodigital/model/cliente/Endereco.java
@@ -0,0 +1,11 @@
+package main.java.br.com.wrsolucoes.bancodigital.model.cliente;
+
+public class Endereco {
+    public String cep;
+    public String rua;
+    public int numero;
+    public String complemento;
+    public String bairro;
+    public String cidade;
+    public String estado;
+}
