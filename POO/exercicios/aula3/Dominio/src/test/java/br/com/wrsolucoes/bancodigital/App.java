bf841f4af35372c110d7e28b1aacfb68034cb7ef
Natan Melo <natan.tere2012@gmail.com>
2021-04-28 09:15:13 -0300

    confirma

---
+++ POO/exercicios/aula3/Dominio/src/test/java/br/com/wrsolucoes/bancodigital/App.java
@@ -0,0 +1,9 @@
+import model.cliente.Cliente;
+
+public class App{
+    public static void main(String[] args) {
+        System.out.println("Iniciando nosso banco digital")
+
+        var cliente = new Cliente();
+    }
+}
