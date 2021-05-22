188dd71cd82a24a51eb2b3e99e83b440edbdc53c
Natan Melo <natan.tere2012@gmail.com>
2021-05-22 05:26:07 -0300

    Create buttons.js

---
+++ Front1/curriculo/public/js/buttons.js
@@ -0,0 +1,72 @@
+function complement() {
+    const info = document.getElementById("complement");
+    const btninfo = document.getElementById("button-complement");
+
+    if (info.style.display == "none") {
+        info.style.display = "inline";
+        btninfo.innerHTML = "<";
+    } else {
+        info.style.display = "none";
+        btninfo.innerHTML = ">";
+    }
+}
+function experience1() {
+    const info = document.getElementById("experience1");
+    const btninfo = document.getElementById("button-experience1");
+
+    if (info.style.display == "none") {
+        info.style.display = "inline";
+        btninfo.innerHTML = "<";
+    } else {
+        info.style.display = "none";
+        btninfo.innerHTML = ">";
+    }
+}
+function experience2() {
+    const info = document.getElementById("experience2");
+    const btninfo = document.getElementById("button-experience2");
+
+    if (info.style.display == "none") {
+        info.style.display = "inline";
+        btninfo.innerHTML = "<";
+    } else {
+        info.style.display = "none";
+        btninfo.innerHTML = ">";
+    }
+}
+function experience3() {
+    const info = document.getElementById("experience3");
+    const btninfo = document.getElementById("button-experience3");
+
+    if (info.style.display == "none") {
+        info.style.display = "inline";
+        btninfo.innerHTML = "<";
+    } else {
+        info.style.display = "none";
+        btninfo.innerHTML = ">";
+    }
+}
+function pessoais() {
+    const info = document.getElementById("pessoais");
+    const btninfo = document.getElementById("button-pessoais");
+
+    if (info.style.display == "none") {
+        info.style.display = "inline";
+        btninfo.innerHTML = "<";
+    } else {
+        info.style.display = "none";
+        btninfo.innerHTML = ">";
+    }
+}
+function outras() {
+    const info = document.getElementById("outras");
+    const btninfo = document.getElementById("button-outras");
+
+    if (info.style.display == "none") {
+        info.style.display = "inline";
+        btninfo.innerHTML = "<";
+    } else {
+        info.style.display = "none";
+        btninfo.innerHTML = ">";
+    }
+}
